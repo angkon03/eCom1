7df4df0524e9c8b539fd2059ef76caeeb41e19b5
angkon <angkon03@gmail.com>
2022-04-04 12:23:16 +0600

    Menu fixed with mobile responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 $('.has-mega-menu').on('click', function(){
     $(this).children('.mega-menu').slideToggle(200);
     $(this).siblings('.has-mega-menu').children('.mega-menu').slideUp(100);
+});
+
+$('.mobile-menu-bars').on('click',function(){
+    $('.main-menu-nav').slideToggle();
+})
+$('.close-btn').on('click',function(){
+    $('.main-menu-nav').slideUp();
 })
